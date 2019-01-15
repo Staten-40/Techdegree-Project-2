47df672ba7bffe74d28f4665625a31b767f43b0d
ShaSta_57 <44626281+Staten-40@users.noreply.github.com>
2019-01-15 01:52:15 -0800

    What happened?
    I did  a lot of copy/pste from Git Hub trying to get my purple button back...and now?  NOTHING IS WORKING!!!!!

--- inc/score.php
+++ inc/score.php
@@ -10,72 +10,67 @@ session_start();
     <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="../css/normalize.css">
     <link rel="stylesheet" href="../css/styles.css">
-<style>
-    body {
-        font-family: 'Playfair Display', serif;
-        background-color: #63D1F4;
-    }
-    .header {
-        margin-left: 38%;
-        margin-top: 5%;
-        font-weight: bold;
-        left: 10%;
-        right: 10%;
-        width: 80%;
-        line-height: .10;
-
-    }
-
-    .header2 {
-        margin-left: -2%;
-        margin-top: 3%;
-        font-weight: bold;
-        left: 10%;
-        right: 10%;
-        line-height: .10;
-
-    a {
-        padding: 10px;
-        border-radius: 10px;
-        appearance: button;
-        text-align: center;
-    }
-
-
-</style>
-   <body>
+    <style>
+        body {
+            font-family: 'Playfair Display', serif;
+            background-color: #63D1F4;
+        }
+        .header {
+            margin-left: 38%;
+            margin-top: 5%;
+            font-weight: bold;
+            left: 10%;
+            right: 10%;
+            width: 80%;
+            line-height: .10;
+        }
+        .header2 {
+            margin-left: -2%;
+            margin-top: 3%;
+            font-weight: bold;
+            left: 10%;
+            right: 10%;
+            line-height: .10;
+        a {
+            padding: 10px;
+            border-radius: 10px;
+            appearance: button;
+            text-align: center;
+         }
+    </style>
+<body>
 
 
 
 <div class = "header">
-<h1>MATH QUIZ</h1>
+    <h1>MATH QUIZ</h1>
 
- <div class = "header2">
- <h3>Mrs. Shoobobbisock's Class</h3>
+    <div class = "header2">
+        <h3>Mrs. Shoobobbisock's Class</h3>
 
-<br><br><br><br><br><br><br><br><br><br><br><br>
+        <br><br><br><br><br><br><br><br><br><br><br><br>
 
-<?php
+        <?php
+        if($_POST["answer"][$_SESSION["whitney"][$_SESSION["counter"]-2]["correctAnswer"]]){
+            echo $_SESSION["results"]++;
+            echo "You got the last question right!";
+            echo "<br>";
+            echo "No more questions";
+            echo "<br>";
+        } else {
+            echo "Your last question was incorrect.  No more questions!";
+            echo "<br>";
+        }
+        ?>
+        <br><br><br><br><br><br><br><br><br>
+        <p>You answered <?php echo $_SESSION["results"]; ?> out of 10 questions correctly.</p>
+        <p>Your score is  <?php echo $_SESSION["results"]*10; ?>%!</p><br>
+        <p>Would you like to give it another go?</p><br>
 
-if($_POST["answer"][$_SESSION]["whitney"][$_SESSION]["counter"]["correctAnswer"]){
-    echo $_SESSION["results"]++;
-    echo "You got the last question right! No more questions!";
-    echo "<br>";
-} else {
-    echo "Your last question was incorrect.  No more questions!";
-    echo "<br>";
-}
 
-?>
- <br><br><br><br><br><br><br><br><br>
-         <p>You answered <?php echo $_SESSION["results"]; ?> out of 10 questions correctly.</p>
-         <p>Your score is  <?php echo $_SESSION["results"]*10; ?>%!</p><br>
-         <p>Would you like to give it another go?</p><br>
+        <a href ="../index.php" type="submit" class="btn"><center>Try Again</center></a>
 
-
-         <a href ="../index.php"  type="submit" class="btn"><center>Try Again</center></a>
-              </div>
-            </div>
-        </div>
-    </body>
+</div>
+</div>
+</body>
 </html>
--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-error_reporting(E_ALL);
+
 session_start();
 include 'inc/questions.php';
 
 if($_SESSION["counter"] == 10) {
-    header("Location: inc/score.php");
-}
+header("Location: inc/score.php");
+
 if(!isset ($_SESSION["counter"]) || ($_SESSION["counter"] > 10))  {
     shuffle($questions);
     $_SESSION["whitney"] = $questions;
@@ -14,11 +14,10 @@ if(!isset ($_SESSION["counter"]) || ($_SESSION["counter"] > 10))  {
 } elseif($_SERVER["REQUEST_METHOD"] == "POST") {
     $_SESSION["counter"]++;
 }
-
 //session_destroy();
 $testQuest = $_SESSION["whitney"][$_SESSION["counter"] -1];
 $response =
-        [
+    [
         $testQuest["correctAnswer"],
         $testQuest["firstIncorrectAnswer"],
         $testQuest["secondIncorrectAnswer"],
@@ -68,11 +67,11 @@ shuffle($response);
 
     </div>
 
-        <div class="toast">
+    <div class="toast">
         <?php
         if($_SESSION["counter"] == 1) {
             echo "<strong>Let's begin!</strong>";
-        } elseif($_POST["answer"] == $_SESSION["whitney"][$_SESSION["counter"]-2]["correctAnswer"]) {
+        } elseif($_POST["answer"] == $_SESSION["whitney"][$_SESSION["counter"]-2]["correctAnswer"])
             echo "<strong>Woo hoo!   You got it right!    Here's the next one:</strong>";
             $_SESSION["results"]++;
         } else {
@@ -83,7 +82,7 @@ shuffle($response);
         <div class="container">
             <div id="quiz-box">
                 <p class="breadcrumbs"><p>Question <?php echo $_SESSION["counter"]; ?> of 10</p>
-                <p class="quiz"><p><b><font size="24"> What is <?php echo $_SESSION["whitney"][$_SESSION["counter"] -1]["leftAdder"]; ?><strong> + </strong> <?php echo $_SESSION["whitney"][$_SESSION["counter"] -1]["rightAdder"]; ?> = ?  </font size></p>
+                <p class="quiz"><p><b><font size="24"> What is <?php echo $_SESSION["whitney"][$_SESSION["counter"] -1]["leftAdder"]; ?><strong>  + </strong> <?php echo $_SESSION["whitney"][$_SESSION["counter"] -1]["rightAdder"]; ?> = ?  </font size></p>
 
                 <form action="index.php" method="post">
                     <input type="hidden" name="id" value="0" />
